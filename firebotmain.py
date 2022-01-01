3650316843f8ca123fef86927762f6b46a50d2f8
SIDDHARTH SRIDHAR <84910325+SRIDHAR2021SIDDHARTH@users.noreply.github.com>
2022-01-01 20:55:20 +0530

    Delete firebotmain.py

--- firebotmain.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.system(
-    "git clone https://github.com/TeamTornadoes/TORNADO-X-USERBOT /root/tornado-xbot && mkdir /root/tornado-xbot/bin/ && cd /root/firebot/ && chmod +x /usr/local/bin/* && python3 -m tornado-xbot"
-)
