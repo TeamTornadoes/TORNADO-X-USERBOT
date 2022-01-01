1e14ec5b8759c6fc6af82bed72b52298d2c9f16e
Devansh <96587665+EvilDevansh@users.noreply.github.com>
2022-01-01 21:00:50 +0530

    update tornado-x-botmain.py

--- tornado-x-botmain.py
+++ tornado-x-botmain.py
@@ -1,5 +1,5 @@
 import os
 
 os.system(
-    "git clone https://github.com/TeamTornadoes/TORNADO-X-USERBOT /root/tornado-xbot && mkdir /root/tornado-xbot/bin/ && cd /root/firebot/ && chmod +x /usr/local/bin/* && python3 -m tornado-xbot"
+    "git clone https://github.com/TeamTornadoes/TORNADO-X-USERBOT /root/tornado-xbot && mkdir /root/tornado-xbot/bin/ && cd /root/tormado-x-bot/ && chmod +x /usr/local/bin/* && python3 -m tornado-x-bot"
 )
